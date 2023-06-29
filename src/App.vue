0b6b2a1c0ba3348a83a09280b17f71df337fb0e3
GLCVLL <g.ioele96@outlook.it>
2023-06-29 16:26:49 +0200

    added Appheader

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,69 @@
 <script>
+// import components
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+// export default
 export default {
   name: 'Dc Comics',
   data() {
-    return [
-      {
-        text: 'Characters',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Comics',
-        url: '#',
-        current: true,
-      },
-      {
-        text: 'Movies',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'TV',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Games',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Collectibles',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Videos',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Fans',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'News',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Shop',
-        url: '#',
-        current: false,
-      },
-    ]
+    return {
+    }
   },
-  components: {},
+  components: { AppHeader, AppMain, AppFooter },
 }
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <!-- header -->
+    <AppHeader />
 
-<style></style>
+  </div>
+</template>
+
+<style>
+/* reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* utilities */
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+/* generals */
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+}
+
+ul li a {
+  text-decoration: none;
+  color: black;
+  padding: 5px;
+}
+
+/* header */
+header {
+  background-color: white;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+
+ul li a:hover {
+  color: #0c7cec;
+}
+</style>
